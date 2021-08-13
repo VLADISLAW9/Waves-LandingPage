a3e934d33548c853cad0e309ff644e704dd076a8
VLADISLAW9 <duplinskij_v_d@mail.ru>
2021-08-13 08:52:19 +0700

    test
    test

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const cursor = document.querySelector(".cursor");
+var timeout;
+
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
+
+ 
+ 
+  clearTimeout(timeout);
+  timeout = setTimeout(mouseStopped, 1000);
+});
+
+
+document.addEventListener("mouseout", () => {
+  cursor.style.display = "none";
+});
